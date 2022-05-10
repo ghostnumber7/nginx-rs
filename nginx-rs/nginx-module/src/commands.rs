88d1cd06b40591a7f6e9964b792b15c74abebc35
Marco Godoy <marcogodoy@untitled.cl>
2022-05-10 14:51:58 +0000

    fix wrong commands count

--- nginx-rs/nginx-module/src/commands.rs
+++ nginx-rs/nginx-module/src/commands.rs
@@ -15,9 +15,11 @@ pub fn expand_commands(module_name: TokenStream, attr: TokenStream) -> Result<To
 
   let conf_ident = format_ident!("ngx_http_{}_module_commands", module_name.to_string());
 
+  let command_length = commands.len() + 1;
+
   let expanded = quote! {
     #[no_mangle]
-    static mut #conf_ident: [ngx_command_t; 3] = [
+    static mut #conf_ident: [ngx_command_t; #command_length] = [
       #(
         #commands::NGX_COMMAND,
       )*
